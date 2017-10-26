bb2d8854f19a07421b126c2e5b3c13e575cb5465
Roberto Vega <robertovegasuay@gmail.com>
2017-10-26 16:24:05 +0200

    Update db.js

--- bd/connection/db.js
+++ bd/connection/db.js
@@ -1,8 +1,6 @@
 //Set up mongoose connection
 var mongoose = require('mongoose');
 
-console.log(process.env);
-
 
 var mongoDB = 'mongodb://vegasuay:Wsxcde10#@cluster0-shard-00-00-cl1il.mongodb.net:27017,cluster0-shard-00-01-cl1il.mongodb.net:27017,cluster0-shard-00-02-cl1il.mongodb.net:27017/todolists?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 mongoose.connect(mongoDB, {
@@ -11,5 +9,6 @@ mongoose.connect(mongoDB, {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function(){
+    alert(process.env);
     console.log('MongoDB connection ok!');
   });
