0489e72cc20d5ffdb73b261a95ccc2ce53089d8c
Roberto Vega <robertovegasuay@gmail.com>
2017-10-26 16:50:36 +0200

    Update db.js

--- bd/connection/db.js
+++ bd/connection/db.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var user=process.env.MONGODB_USER;
 var pass=process.env.MONGODB_PASS;
 
-var mongoDB = 'mongodb://vegasuay:Wsxcde10#@cluster0-shard-00-00-cl1il.mongodb.net:27017,cluster0-shard-00-01-cl1il.mongodb.net:27017,cluster0-shard-00-02-cl1il.mongodb.net:27017/todolists?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+var mongoDB = 'mongodb://'+user+':'+pass+'@cluster0-shard-00-00-cl1il.mongodb.net:27017,cluster0-shard-00-01-cl1il.mongodb.net:27017,cluster0-shard-00-02-cl1il.mongodb.net:27017/todolists?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 mongoose.connect(mongoDB, {
     useMongoClient: true
 });
