be2c3051a0139eed3b71a46d8e71cbdb72a0cc3b
Roberto Vega <roberto.vegas@tecnocom.es>
2017-11-02 11:57:33 +0100

    authentication

--- bd/connection/db.js
+++ bd/connection/db.js
@@ -3,11 +3,7 @@ var mongoose = require('mongoose');
 var user=process.env.MONGODB_USER;
 var pass=process.env.MONGODB_PASS;
 
-<<<<<<< HEAD
-var mongoDB = 'mongodb://user:pass@cluster0-shard-00-00-cl1il.mongodb.net:27017,cluster0-shard-00-01-cl1il.mongodb.net:27017,cluster0-shard-00-02-cl1il.mongodb.net:27017/todolists?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
-=======
 var mongoDB = 'mongodb://'+user+':'+pass+'@cluster0-shard-00-00-cl1il.mongodb.net:27017,cluster0-shard-00-01-cl1il.mongodb.net:27017,cluster0-shard-00-02-cl1il.mongodb.net:27017/todolists?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
->>>>>>> 0489e72cc20d5ffdb73b261a95ccc2ce53089d8c
 mongoose.connect(mongoDB, {
     useMongoClient: true
 });
